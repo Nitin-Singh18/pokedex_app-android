578643b664b7a429d376cf7244a03fb94c61b78e
Nitin-Singh18 <i.nitinsingh18@gmail.com>
2023-12-20 17:40:58 +0530

    navigation set up

---
+++ app/src/main/java/com/example/pokedexapp/PokemondexApplication.kt
@@ -0,0 +1,13 @@
+package com.example.pokedexapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class PokeDexApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
